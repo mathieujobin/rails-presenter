d9cd8a004dea3d742366da778aa51f192153c4b5
Mathieu Jobin <mathieu@justbudget.com>
2014-12-23 12:31:31 -0500

    basic controller with first two tests

--- support/test_app/config/routes.rb
+++ support/test_app/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'test/index' => 'test#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
---
+++ support/test_app/spec/test_controller_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+RSpec.describe TestController, :type => :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq 200
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    #it "loads all of the posts into @posts" do
+    #  post1, post2 = Post.create!, Post.create!
+    #  get :index
+    #
+    #  expect(assigns(:posts)).to match_array([post1, post2])
+    #end
+  end
+end
